b4dcd687dee752ae9df89be3d80ed949172809a2
Kimaya0310 <cskimaya03@gmail.com>
2026-07-02 19:37:53 +0530

    Fix Cloudinary env loading order in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,10 @@
+import "./config/loadEnv.js";
 import express from "express";
-import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
 
-dotenv.config();
 connectDB();
 
 const app = express();
